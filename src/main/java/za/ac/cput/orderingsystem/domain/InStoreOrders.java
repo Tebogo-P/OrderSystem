fd37986a181dfc31f5eba31ee28cacef49105d8e
Tebogo <notorioustee11@gmail.com>
2026-05-06 00:32:03 +0200

    Initial Commit

--- src/main/java/za/ac/cput/orderingsystem/domain/InStoreOrders.java
+++ src/main/java/za/ac/cput/orderingsystem/domain/InStoreOrders.java
@@ -1,5 +1,6 @@
 package za.ac.cput.orderingsystem.domain;
 
+import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.Entity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -7,6 +8,7 @@ import lombok.EqualsAndHashCode;
 @Entity
 @Data
 @EqualsAndHashCode(callSuper=true)
+@DiscriminatorColumn
 
 public class InStoreOrders extends Orders {
     private double immediatePayment;
--- src/main/java/za/ac/cput/orderingsystem/domain/Orders.java
+++ src/main/java/za/ac/cput/orderingsystem/domain/Orders.java
@@ -7,6 +7,7 @@ import java.util.Date;
 
 @Data
 @MappedSuperclass
+@Inheritance(strategy=InheritanceType.JOINED)
 
 public abstract class Orders{
     @Id
